e9be4c755b1cf9314afd9ba79e117d4289cac808
root <root@p-eqmgih0kxw3a-workstation.c.instruqt-prod.internal>
2020-12-09 06:06:41 +0000

    commit

---
+++ outputs.tf
@@ -0,0 +1,8 @@
+# Outputs file
+output "catapp_url" {
+  value = "http://${aws_eip.hashicat.public_dns}"
+}
+
+output "key_pem" {
+  value = tls_private_key.hashicat.private_key_pem
+}
---
+++ remote.tf
@@ -0,0 +1,19 @@
+terraform {
+backend "remote" {
+organization = "terraform-workshop-fujitsu"
+workspaces {
+name = "dev-ochir-hashicat-app"
+}
+}
+}
+
+/*
+terraform {
+  backend "remote" {
+    organization = "masa_org"
+    workspaces {
+      name = "hashicat-app"
+    }
+  }
+}
+*/
